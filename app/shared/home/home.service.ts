958cc6c0f1c8abc3a59216d44e263d20613872ef
andermev <ander1298@gmail.com>
2018-09-06 00:13:56 -0500

    Refactor code

--- app/shared/home/home.service.ts
+++ app/shared/home/home.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from "@angular/core";
-import { Notification } from "~/model/notification.model";
 
 @Injectable()
 export class HomeService {
     getNotifications(): any {
         const firebase = require("nativescript-plugin-firebase/app");
-        const citiesCollection = firebase.firestore().collection("alerts");
+        const alertsCollection = firebase.firestore().collection("alerts");
 
         const notifications = [];
-        citiesCollection.get().then((querySnapshot) => {
+        alertsCollection.get().then((querySnapshot) => {
             querySnapshot.forEach((doc) => {
                 console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
                 notifications.push(doc);
