b8bf9648eafc9ef31e0d25cf9bef515b61f9ad5a
andermev <ander1298@gmail.com>
2018-09-08 12:03:26 -0500

    Remodified form to get data from database, now it's used observables with async prompt.

--- app/component/notification/notification.component.ts
+++ app/component/notification/notification.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { RouterExtensions } from "nativescript-angular/router";
+import { Observable } from "rxjs";
+import { Notification } from "~/models/notification.model";
 import { WarningService } from "~/shared/warning/warning.service";
 
 @Component({
@@ -8,17 +10,19 @@ import { WarningService } from "~/shared/warning/warning.service";
     templateUrl: "./notification.component.html"
 })
 export class NotificationComponent implements OnInit {
-    items: any;
+    items = [];
+    myCities$: Observable<Array<Notification>>;
 
     constructor(private warningService: WarningService, private router: RouterExtensions) { }
 
     ngOnInit(): void {
-        const notifications = [];
-        this.warningService.getNotifications()
-            .subscribe((data) => data.forEach((doc) => {
-                console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
-                notifications.push(doc);
-            }),
-            () => console.log("Error Get Notification"));
+        // this.warningService.getNotifications()
+        //     .subscribe((data) => data.forEach((doc) => {
+        //         console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+        //         this.items.push(doc.data());
+        //     }),
+        //     () => console.log("Error Get Notification"))
+        //     .catch((err) => console.log("Get failed, error: " + err));
+        this.myCities$ = this.warningService.firestoreCollectionObservable();
     }
 }
--- app/shared/warning/warning.service.ts
+++ app/shared/warning/warning.service.ts
@@ -1,12 +1,35 @@
 import { Injectable, NgZone } from "@angular/core";
-import { from } from "rxjs";
+import { firestore } from "nativescript-plugin-firebase";
+import { from, Observable } from "rxjs";
+import { Notification } from "~/models/notification.model";
 
 const firebase = require("nativescript-plugin-firebase/app");
 
 @Injectable()
 export class WarningService {
+    notification: Notification;
+
+    private cities: Array<Notification> = [];
+
+    constructor(private zone: NgZone) { }
 
     getNotifications(): any {
         return from(firebase.firestore().collection("alerts").get());
+    }
+
+    firestoreCollectionObservable(): Observable<Array<Notification>> {
+        return Observable.create((subscriber) => {
+            const colRef: firestore.CollectionReference = firebase.firestore().collection("alerts");
+            colRef.onSnapshot((snapshot: firestore.QuerySnapshot) => {
+            this.zone.run(() => {
+                this.cities = [];
+                snapshot.forEach((docSnap) => {
+                    console.log("Document Firebase" + `${docSnap.id} => ${JSON.stringify(docSnap.data())}`);
+                    this.cities.push(new Notification(docSnap.data()));
+                });
+                subscriber.next(this.cities);
+            });
+            });
+        });
     }
 }
